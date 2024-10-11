39014a59083c2d5dc286c6c472f466f5ab6dce46
thiru2612 <rthiruppathi618@gmail.com>
2024-10-11 16:49:21 +0530

    commited

--- E-commerce/src/pages/Collection.jsx
+++ E-commerce/src/pages/Collection.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/frontend_assets/assets';
+import Title from '../components/Title';
 
 const Collection = () => {
 
@@ -10,7 +12,9 @@ const Collection = () => {
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/*Filter Options*/}
       <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        <p onClick={()=>setFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+            <img className={`h-3 sm:hidden ${showFilter?'rotate-90':''}`} src={assets.dropdown_icon} alt="" />
+        </p>
         {/* Category filter */}
           <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter? '':'hidden'} sm:block`}> 
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
@@ -27,7 +31,7 @@ const Collection = () => {
           </div>
         </div> 
         {/* Subcategory */}
-          <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter? '':'hidden'} sm:block`}> 
+          <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter? '':'hidden'} sm:block`}> 
           <p className='mb-3 text-sm font-medium'>Type</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
@@ -42,6 +46,13 @@ const Collection = () => {
           </div>
         </div> 
       </div>
+
+        <div className='flex-1'>
+            <div className='flex justify-between text-base sm:text-2xl mb-4'>
+                <Title text1={'ALL '} text2={'COLLECTIONS'}/>
+            </div>
+        </div>
+
     </div>
   )
 }
